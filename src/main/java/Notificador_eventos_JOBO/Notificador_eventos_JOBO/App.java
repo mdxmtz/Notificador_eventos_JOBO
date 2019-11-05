3b4bf60f2963548efbfd47ae265dd0691abfc943
Pablo T Campos <55254526+burned301@users.noreply.github.com>
2019-11-05 19:57:34 +0100

    Initial commit

---
+++ src/main/java/Notificador_eventos_JOBO/Notificador_eventos_JOBO/App.java
@@ -0,0 +1,50 @@
+package Notificador_eventos_JOBO.Notificador_eventos_JOBO;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        final String username = "anunciosjobo@gmail.com";
+        final String password = "I9H%8Lo!!OOl16";
+
+        Properties prop = new Properties();
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true"); //TLS
+        
+        Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse("pablosky301@gmail.com, pablotcampos25@gmail.com, 100405806@alumnos.uc3m.es")
+            );
+            message.setSubject("Testing Gmail TLS");
+            message.setText("Dear Mail Crawler,"
+                    + "\n\n Please do not spam my email!");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
