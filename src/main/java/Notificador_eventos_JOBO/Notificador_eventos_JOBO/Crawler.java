02d1c75cc146b49c0132a3816fbe790be306bd56
Pablo T Campos <55254526+burned301@users.noreply.github.com>
2019-11-13 09:39:10 +0100

    Crawler updated

--- src/main/java/Notificador_eventos_JOBO/Notificador_eventos_JOBO/Crawler.java
+++ src/main/java/Notificador_eventos_JOBO/Notificador_eventos_JOBO/Crawler.java
@@ -1,12 +1,12 @@
 package Notificador_eventos_JOBO.Notificador_eventos_JOBO;
 
+import org.jetbrains.annotations.NotNull;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.pagefactory.ByAll;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Crawler {
 
@@ -44,27 +44,57 @@ public class Crawler {
         //List<WebElement> eventos = driver.findElements(By.cssSelector("input[type=EVENT]"));
 
         System.out.println( eventos.isEmpty());
+        List<Evento> eventosScrap = new ArrayList<Evento>(); //new List<Evento>();
 
         for(WebElement evento : eventos){
             if(evento.getAttribute("id").contains("prod_")){
+                int id = ID(evento);
                 String title = title(evento);
-                System.out.println(title);
-                System.out.println("____");
-
+                String date = date(evento);
+                String place = place(evento);
+                Boolean agotado = agotado(evento);
+                Evento event = new Evento(id,title,date,place,agotado);
+                eventosScrap.add(event);
             }
         }
+        for(Evento evento: eventosScrap){
+            evento.showEvento();
+            System.out.println("________");
+        }
+        driver.close();
+    }
 
+    public int ID(WebElement evento){
+        String[] s = evento.getAttribute("id").split("prod_");
+        int result = Integer.parseInt(s[1].trim());
+        return result;
+    }
 
-        /*driver.get("http://demo.guru99.com/");
-        WebElement element=driver.findElement(By.xpath("//input[@name='emailid']"));
-        element.sendKeys("abc@gmail.com ");
-
-        WebElement button=driver.findElement(By.xpath("//input[@name='btnLogin']"));
-        button.click();*/
+    public String date(WebElement evento){
+        List<WebElement> h = evento.findElements(By.className("date"));
+        return h.get(0).getText().trim();
     }
 
     public String title(WebElement evento){
-        List<WebElement> h = evento.findElements(By.className("date"));
+        List<WebElement> h = evento.findElements(By.className("title"));
         return h.get(0).getText().trim();
+    }
+
+    public String place(WebElement evento){
+        //List<WebElement> h = evento.findElements(By.className("location"));
+        //return h.get(1).getText().trim();
+        List<WebElement> spaces = evento.findElements(By.className("space"));
+        List<WebElement> sites = evento.findElements(By.className("site"));
+        String space = spaces.get(1).getText().trim();
+        String site = sites.get(1).getText().trim();
+        String result = space + ", " + site;
+
+        return result;
+
+    }
+
+    public Boolean agotado(WebElement evento){
+        if(evento.getText().contains("Agotado"))return true;
+        return false;
     }
 }
