75d6950dd78fad04b7114d2b30d0ae64ee9b7e5c
Pablo T Campos <55254526+burned301@users.noreply.github.com>
2019-12-08 14:33:50 +0100

    App updated

--- src/main/java/Notificador_eventos_JOBO/Notificador_eventos_JOBO/App.java
+++ src/main/java/Notificador_eventos_JOBO/Notificador_eventos_JOBO/App.java
@@ -1,12 +1,11 @@
 package Notificador_eventos_JOBO.Notificador_eventos_JOBO;
-
+import java.sql.*;
 
 public class App {
 
     public static void main(String[] args) {
         Crawler crawler = new Crawler();
         crawler.start();
-
     }
 }
 
